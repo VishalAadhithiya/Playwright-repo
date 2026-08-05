50601d69477466277f692d3f6c71fbf11dfdc075
Vishal Aadhithiya <151649798+VishalAadhithiya@users.noreply.github.com>
2026-08-05 23:58:05 +0530

    week1day2 home assignment

---
+++ homeassgn.js
@@ -0,0 +1,12 @@
+let userName = "Vishal"
+let organisation = "Qeagle"
+let contactNumber = 1234567890
+let isKnowsAutomation = true
+let usesPlaywright
+
+console.log(userName + " :" + typeof userName)
+console.log(organisation + " :" + typeof organisation)
+console.log(contactNumber + " :" + typeof contactNumber)
+console.log(isKnowsAutomation + " :" + typeof isKnowsAutomation)
+console.log(usesPlaywright + " :" + typeof usesPlaywright)
+
