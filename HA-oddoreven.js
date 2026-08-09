0392744aa1f5b325e6e6041ad062a042cd716ffd
Vishal Aadhithiya <151649798+VishalAadhithiya@users.noreply.github.com>
2026-08-09 19:42:43 +0530

    Add files via upload
    odd or even

---
+++ HA-oddoreven.js
@@ -0,0 +1,10 @@
+function isOddOrEven(num) {
+    if (num % 2 === 0) {
+        return "even";
+    }
+    else{
+        return "odd";
+    }
+}
+console.log(isOddOrEven(5));
+console.log(isOddOrEven(10));
